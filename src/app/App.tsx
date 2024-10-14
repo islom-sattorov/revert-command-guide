2f3d9f8b4f90f44eec080f83f73eac144e1b2179
islom-sattorov <slmsttrv1@gmail.com>
2024-10-14 18:29:36 +0500

    commit 1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
 export function App() {
+  console.log("commit 1");
   return (
     <AuthProvider>
       <RouterProvider router={router} />
